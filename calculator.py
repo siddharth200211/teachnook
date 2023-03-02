1c3e7463cc5efb54313fad7eac216e2e8e25ef27
Siddharth_Patil <125746301+siddharth200211@users.noreply.github.com>
2023-03-02 14:35:16 +0530

    ass1

--- calculator.py
+++ calculator.py
@@ -28,7 +28,7 @@ while True:
     f.close()
     
 
-'''
+#part 2
 #list examples
 A=[34,56,12]
 print(A)
@@ -73,9 +73,90 @@ print(qbank[1]["quest"])
 print(qbank[1]["opt"])
 print(qbank[1]["cans"])
 
+#part 3
+f=open("sid.txt","w") #w..to create a new file and allow to write in the file
+f.write("Two")
+f.close()
+print("File created")
+
+f=open("sid.txt","w")
+while True:
+    txt=input("Enter text")
+    if txt=='end':
+        break
+    f.write(txt+"\n")
+f.close()
+
+
+# 'r' mode
+f=open("sid.txt","r") #r...to read the file
+st=f.read()
+print(st)
+print(type(st))
+
+# 'a' mode
+f=open("sid.txt","a") #to add records in existing file
+f.write("DBMS\n")
+f.write("UNIX\n")
+f.close()
+
+
+
+#examples
+#wap to store roll no,name and marks of p,c,m in a file with name
+#'stud.txt'
+f=open('stud.txt','w')
+while True:
+    rno=input("Roll no?")
+    nm=input("Name?")
+    p=input("Physics marks?")
+    c=input("Chemistry marks?")
+    m=input("Math marks?")
+    rec=rno+","+nm+","+p+","+c+","+m+"\n"
+    f.write(rec)
+    if input("Continue?")=='no':
+        break
+f.close()
+print("File created")
+f=open('stud.txt','r')
+st=f.readlines()
+f.close()
+#print(st)
+student=[]
+for rec in st:
+    each_stud=rec.split(",")
+    #print(each_stud)
+    each_stud[2]=int(each_stud[2])
+    each_stud[3]=int(each_stud[3])
+    each_stud[4]=int(each_stud[4][:-1])
+    #print(each_stud)
+    student.append(each_stud)
+print(student)
+for S in student:
+    tot=S[2]+S[3]+S[4]
+    per=tot/3
+    if S[2]>=35 and S[3]>=35 and S[4]>=35:
+        rem='Pass'
+        if per>=60:
+            grade='A'
+        elif per>=45:
+            grade='B'
+        else:
+            grade='C'
+    else:
+        rem='Fail'
+        grade='-'
+    print("Roll No:",S[0])
+    print("Name:",S[1])
+    print("Marks:")
+    print("Physics\tChemistry\tMaths")
+    print(S[2],"\t",S[3],"\t","\t",S[4])
+    print("Total:",tot,"\tPercentage:",per)
+    print("Remark:",rem,"\tGrade:",grade)
 
 
 
 
 
-'''
+
+
